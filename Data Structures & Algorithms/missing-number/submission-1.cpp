579312416adab3904f97df503c046809958743f2
Riddhi256 <145008423+Riddhi256@users.noreply.github.com>
2026-08-04 23:47:15 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=1;i<n+1;i++)
+        {
+            ans^=i;
+        }
+        for(int x: nums)
+        {
+            ans^=x;
+        }
+        return ans;
+    }
+};
